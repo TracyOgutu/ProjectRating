8fbb214c6758ba272735fa5cc1dabdd5e1f95e7d
Tracy Ogutu <tracyogutu@gmail.com>
2020-01-12 17:55:19 +0300

    change contact field in Profile Class to CharField

--- ratings/models.py
+++ ratings/models.py
@@ -9,11 +9,9 @@ class Profile(models.Model):
     '''
     profile_photo=models.ImageField(upload_to='images/',blank=True)
     bio=models.CharField(max_length=100)
-    contact=models.IntegerField()
+    contact=models.CharField(max_length=25)
     editor = models.ForeignKey(User,on_delete=models.CASCADE)
     
-   
-
     def __str__(self):
         '''
         Setting up self 
@@ -40,6 +38,22 @@ class Profile(models.Model):
         '''
         self.delete()
 
+    @classmethod
+    def single_profile(cls,user_id):
+        '''
+        function gets a single profile posted by id
+        '''
+        profile=cls.objects.filter(editor=user_id)
+        return profile
+
+    @classmethod
+    def get_profilepic_id(cls,imageId):
+        '''
+        function that gets a profilepic id    
+        '''
+        image_id=cls.objects.filter(id=imageId)
+        return image_id
+
 class Project(models.Model):
     title=models.CharField(max_length=30)
     image=models.ImageField(upload_to='images/',blank=True)
@@ -47,4 +61,64 @@ class Project(models.Model):
     link=models.URLField( max_length=128, db_index=True,unique=True,blank=True)
     editor = models.ForeignKey(User,on_delete=models.CASCADE)
     profile=models.ForeignKey(Profile,null=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        '''
+        Setting up self
+        '''
+        return self.title
+
+    def save_project(self):
+        '''
+        Method for saving the project
+        '''
+        self.save()
+
+    def delete_project(self):
+        '''
+        Method for deleting the project
+        '''
+        self.delete()
+    
+    @classmethod
+    def get_projects(cls):
+        '''
+        Method for retrieving all images
+        '''
+        project=cls.objects.all()
+        return project
+
+    @classmethod
+    def user_projects(cls,user_id):
+        '''
+        function gets projects posted by id
+        '''
+        project_posted=cls.objects.get(editor=user_id)
+        return project_posted    
+
+    @classmethod
+    def search_by_title(cls,tag):
+        '''
+        Method for searching for a project using the title
+        '''
+
+        search_result=cls.objects.filter(title__icontains=tag)
+        return search_result
+
+    @classmethod
+    def single_project(cls,project_id):
+        '''
+        function gets a single project posted by id
+        '''
+        project_posted=cls.objects.get(id=project_id)
+        return project_posted
+
+    @classmethod
+    def get_image_id(cls,imageId):
+        '''
+        function that gets an image id    
+        '''
+        image_id=cls.objects.filter(id=imageId)
+        return image_id
 
