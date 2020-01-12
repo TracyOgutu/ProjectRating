d6e889b5dcefa835a8e77ea78bc64ebfb113db90
Tracy Ogutu <tracyogutu@gmail.com>
2020-01-12 17:56:00 +0300

    add search and display profile url

--- ratings/urls.py
+++ ratings/urls.py
@@ -6,16 +6,16 @@ from . import views
 
 urlpatterns=[
     url('^$',views.home,name='home'),
-    # url(r'^search/',views.search_category,name='search_category'),
-    # url(r'^photo/(\d+)',views.single_photo,name='photo'),
-    # url(r'^new/image$', views.new_image, name='new-image'),
-    # url(r'^new/profile$', views.new_profile, name='new-profile'),
+    url(r'^search/',views.search_title,name='search_title'),
+    url(r'^project/(\d+)',views.single_project,name='project'),
+    url(r'^add/project$', views.add_project, name='add-project'),
+    url(r'^new/profile$', views.new_profile, name='new-profile'),
     # url(r'^updateprofile$',views.updateprofile,name='updateprofile'),
     # url(r'^comment$',views.makecomment,name='makecomment'),
     # url(r'^like$',views.like_a_post,name='like_a_post'),
     # url(r'^follow$',views.follow,name='follow'),
     # url(r'^delete/(?P<post_id>\d+)$',views.delete_post,name='delete'),
-    # url(r'displayprofile/(?P<user_id>\d+)$',views.display_profile,name='displayprofile')
+    url(r'displayprofile/(?P<user_id>\d+)$',views.display_profile,name='displayprofile')
     
     
 ]
