f48d1999627570e6d5c7131a7d40f6351ea910f0
Tracy Ogutu <tracyogutu@gmail.com>
2020-01-13 13:56:24 +0300

    add restframework

--- ratings/views.py
+++ ratings/views.py
@@ -5,10 +5,33 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
 from .forms import NewProjectForm,NewProfileForm
-from .models import Project,Profile
+from .models import Project,Profile,Rating
+from .serializer import ProfileSerializer,ProjectSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+class ProfileList(APIView):
+    '''
+    End point that returns all the profile details such as bio,
+    profile_pic,projects posted and contact information
+    '''
+    def get(self, request, format=None):
+        all_profile = Profile.objects.all()
+        serializers = ProfileSerializer(all_profile, many=True)
+        return Response(serializers.data)
+
+class ProjectList(APIView):
+    '''
+    End point that returns all projects posted and the details such as title,
+    image,description and live link to the project
+    '''
+    def get(self, request, format=None):
+        all_project = Project.objects.all()
+        serializers = ProjectSerializer(all_project, many=True)
+        return Response(serializers.data)
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def home(request):
     '''
     View for the main homepage.
@@ -20,8 +43,8 @@ def home(request):
         profile=Profile.objects.filter(editor=logged_in_user)
     except Profile.DoesNotExist:
         profile=None
-    return render(request,'home.html',{"projects":logged_in_user_projects,"profile":profile,"allprojects":all_projects})
-
+    return render(request,'testhome.html',{"projects":logged_in_user_projects,"profile":profile,"allprojects":all_projects})
+@login_required(login_url='/accounts/login/')
 def add_project(request):
     current_user = request.user
     if request.method == 'POST':
@@ -36,7 +59,7 @@ def add_project(request):
         form = NewProjectForm()
     return render(request, 'new_project.html', {"form": form})
 
-
+@login_required(login_url='/accounts/login/')
 def single_project(request,project_id):
     '''
     This method displays a single photo and its details such as comments, date posted and caption
@@ -44,8 +67,14 @@ def single_project(request,project_id):
 
     project_posted=Project.single_project(project_id)  
     imageId=Project.get_image_id(project_id)
-   
-        
+    rating=Rating.get_rating_byproject_id(project_id)
+
+    design=Rating.design
+    usability=Rating.usability
+    content=Rating.content
+    
+
+    
     # try:
     #     photo=Image.objects.get(id=photo_id)
 
@@ -54,7 +83,7 @@ def single_project(request,project_id):
 
     return render(request,'project.html',{"project":project_posted})
 
-
+@login_required(login_url='/accounts/login/')
 def new_profile(request):
     '''
     Used for creating a new profile for the user. It includes a profile photo, a bio and contact 
@@ -72,7 +101,7 @@ def new_profile(request):
         form = NewProfileForm()
     return render(request, 'new_profile.html', {"form": form})
 
-
+@login_required(login_url='/accounts/login/')
 def display_profile(request,user_id):
     '''
     View for displaying a single profile
@@ -83,8 +112,11 @@ def display_profile(request,user_id):
         return render(request,'profiledisplay.html',{"profile":single_profile,"projects":projects_posted})
     except Profile.DoesNotExist:
         messages.info(request,'The user has not set a profile yet')
+    except Project.DoesNotExist:
+        messages.info(request,'The user has not posted a project yet')
         return redirect('home')
 
+@login_required(login_url='/accounts/login/')
 def search_title(request):
     '''
     This method searches for an image by using the name of the image
@@ -98,3 +130,15 @@ def search_title(request):
     else:
         message="You haven't searched for any term"
         return render(request,'search.html',{"message":message})
+        
+@login_required(login_url='/accounts/login/')
+def add_rating(request):
+    if request.method == "POST":      
+        design = request.POST.get("design", None)
+        usability = request.POST.get("usability", None)
+        content = request.POST.get("content", None) 
+    
+    return render(request,'rate.html')
+
+
+
